a63e4def8e28f6313e535a9aef13f7958937f5c3
ChyenneW <williamscheyenne771@gmail.com>
2022-05-04 04:52:12 -0500

    Created rough frame of app

--- src/App.js
+++ src/App.js
@@ -1,23 +1,30 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <div>Day</div>
+        <div>Week</div>
+        <div>Month</div>
+        <div>Year</div>
       </header>
+      <div>
+        <div>Thursday</div>
+        <div>March 23, 2017</div>
+      </div>
+      <form>
+        <input type="text" placeholder="Enter a Task" />
+      </form>
+      <div>pinned tasks</div>
+      <hr />
+      <div>
+        <ul>
+          <li>task</li>
+          <li>task</li>
+          <li>task</li>
+        </ul>
+      </div>
     </div>
   );
 }
