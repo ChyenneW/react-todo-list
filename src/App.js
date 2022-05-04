ab6b3c32a34f3cb5d7e46ab26055d2c80d6f9f75
ChyenneW <williamscheyenne771@gmail.com>
2022-05-04 06:14:23 -0500

    Updated HTML frame

--- src/App.js
+++ src/App.js
@@ -1,24 +1,33 @@
+import "bootstrap/dist/css/bootstrap.min.css";
 import "./App.css";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <div>Day</div>
-        <div>Week</div>
-        <div>Month</div>
-        <div>Year</div>
+      <header className="row App-header">
+        <div className="col-2 active">Day</div>
+        <div className="col-2">Week</div>
+        <div className="col-2">Month</div>
+        <div className="col-2">Year</div>
       </header>
-      <div>
-        <div>Thursday</div>
-        <div>March 23, 2017</div>
+      <div className="row date">
+        <div className="col-1">⇐</div>
+        <div className="col-6">
+          <div>Thursday</div>
+          <div>March 23, 2017</div>
+        </div>
+        <div className="col-1">⇒</div>
       </div>
-      <form>
+      <form className="taskForm">
         <input type="text" placeholder="Enter a Task" />
       </form>
-      <div>pinned tasks</div>
-      <hr />
-      <div>
+      <div className="pinnedTasks">
+        <ul>
+          <li>pinned tasks</li>
+        </ul>
+      </div>
+      <hr className="pageLiner" />
+      <div className="tasks">
         <ul>
           <li>task</li>
           <li>task</li>
